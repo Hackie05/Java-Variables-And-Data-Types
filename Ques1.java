ca17baa9345c03c9d942b268047b20fb0d3cf028
Hackie05 <116246832+Hackie05@users.noreply.github.com>
2023-01-06 18:48:56 +0530

    Create Ques1.java
    Q1- Take two integer values in 2 variables x and y and then print their products.

---
+++ Ques1.java
@@ -0,0 +1,10 @@
+import java.until.*;
+class ques1{
+   public static void main(String arts[]){
+      Scanner sc = new Scanner(System.in);
+      System.out.println("Enter two numbers");
+      int x = sc.nextInt();
+      int y = sc.nextInt();
+      System.out.println("The product of the two numbers is: " + x*y);
+}
+}
