5e74088e1eaa42ea1fbf4f14e2233c2070e9816c
Hackie05 <116246832+Hackie05@users.noreply.github.com>
2023-01-06 19:04:02 +0530

    Create Ques4.java
    Q4- Enter a no. then print its cube.

---
+++ Ques4.java
@@ -0,0 +1,9 @@
+import java.until.*;
+class Ques3{
+      public static void main(String args[]){
+         Scanner sc = new Scanner(System.in);
+         System.out.print("Enter a no. : ");
+         int n = sc.nextInt();
+         System.out.println("The cube of the entered no. is : " + n*n*n);
+}
+}
