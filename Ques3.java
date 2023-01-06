35ea31445098780ed5c6564e90e5bad84e151040
Hackie05 <116246832+Hackie05@users.noreply.github.com>
2023-01-06 19:00:48 +0530

    Create Ques3.java
    Q3- WAP to take length and breadth of a rectangle and prints its area.

---
+++ Ques3.java
@@ -0,0 +1,10 @@
+import java.until.*;
+class Ques3{
+      public static void main(String args[]){
+         Scanner sc = new Scanner(System.in);
+         System.out.println("Enter the length and breadth of a rectangle");
+         int l = sc.nextInt();
+         int b = sc.nextInt();
+         System.out.println("The area of rectangle is : " + l*b);
+}
+}
